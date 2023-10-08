928aad0f05d7c7e4ebcc61ad1689af6417fafc09
i-amdash <ashimidoyin@yahoo.com>
2023-10-08 23:33:39 +0100

    switched to naviagtion

---
+++ app/(pages)/(routes)/exhibition/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/Header';
+
+const Exhibition =  async () => {
+    return (
+    <>
+    <Header />
+    Exhibition
+      </>
+    );
+  };
+  
+  export default Exhibition;
