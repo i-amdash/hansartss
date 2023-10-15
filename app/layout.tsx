cb2e25e4277c581dfad1f33c9d034b183ce6bced
i-amdash <ashimidoyin@yahoo.com>
2023-10-15 10:07:22 +0100

    few more settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default async function RootLayout({
           <div>
           <ThemeProvider 
             attribute="class" 
-            defaultTheme="dark" 
+            defaultTheme="system" 
             enableSystem
           >
             <ToastProvider />
