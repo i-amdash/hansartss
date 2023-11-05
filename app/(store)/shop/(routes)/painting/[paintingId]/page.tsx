56aa7d9612c6216d69b9c6806cb8e4a43b928d4c
i-amdash <ashimidoyin@yahoo.com>
2023-11-05 19:58:24 +0100

    painting detail

--- app/(store)/shop/(routes)/painting/[paintingId]/page.tsx
+++ app/(store)/shop/(routes)/painting/[paintingId]/page.tsx
@@ -26,7 +26,7 @@ const PaintingPage: React.FC<PaintingPageProps> = async ({
   }
 
   return (
-    <div className="bg-white">
+    <div className="bg-secondary">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
           <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
