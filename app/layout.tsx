93bba1f469b3b2c82501810fe9ac92ba288f8b5b
i-amdash <ashimidoyin@yahoo.com>
2023-11-18 20:05:46 +0100

    changed the site description

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,7 +20,7 @@ const sora = Sora({
 
 export const metadata = {
   title: 'Hans Artss',
-  description: 'A Pantheon of Beautiful Thoughts',
+  description: 'The Corporate Artist',
   icons: {
     icon: '/icon.jpg',
   },
